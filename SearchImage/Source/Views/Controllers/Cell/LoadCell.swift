42e3c9774005029cd714785cfd91ce6158bb265a
sbear123 <sbear123@dgsw.hs.kr>
2021-06-26 20:42:43 +0900

    [UPDATE] Cell수정

---
+++ SearchImage/Source/Views/Controllers/Cell/LoadCell.swift
@@ -0,0 +1,18 @@
+//
+//  SearchCollectionReusableView.swift
+//  SearchImage
+//
+//  Created by 박지현 on 2021/06/26.
+//
+
+import UIKit
+
+public class LoadCell: UICollectionReusableView {
+  override init(frame: CGRect) {
+    super.init(frame: frame)
+  }
+
+  required init?(coder aDecoder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+}
