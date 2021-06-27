25f5c942876f5ab2d05898215fa67e1133ceb972
sbear123 <sbear123@dgsw.hs.kr>
2021-06-27 16:06:52 +0900

    [UPDATE] Error난 것 확인 하는 코드 변경

--- SearchImage/Source/Services/MainService.swift
+++ SearchImage/Source/Services/MainService.swift
@@ -11,8 +11,8 @@ import RxCocoa
 
 protocol MainServiceProtocol {
     var apiManager: APIManager { get }
-    func getDocuments(_ search: String) -> Observable<Search>
-    func updateDocuments(_ search: Search, text: String) -> Observable<Search>
+    func getDocuments(_ search: String, handler: @escaping (Search, Bool) -> Void)
+    func fetchDocuments(_ search: Search, text: String, handler: @escaping (Search, Bool) -> Void)
 }
 
 class MainService: MainServiceProtocol {
@@ -20,21 +20,34 @@ class MainService: MainServiceProtocol {
     let disposeBag = DisposeBag()
     var page: Int = 1
     
-    func getDocuments(_ text: String) -> Observable<Search> {
+    func getDocuments(_ text: String, handler: @escaping (Search, Bool) -> Void) {
+        var result: Search = Search(meta: Meta(), document: [])
         page = 1
         let param: [String:Any] = ["query":text, "page":page, "size":30]
-        return apiManager.getAPI(param: param)
+        apiManager.getAPI(param: param).subscribe(
+            onNext: { data in
+                result = data
+                handler(result, true)
+            },
+            onError: { err in
+                print(err)
+                handler(result, false)
+            }).disposed(by: disposeBag)
     }
     
-    func updateDocuments(_ search: Search, text: String) -> Observable<Search> {
+    func fetchDocuments(_ search: Search, text: String, handler: @escaping (Search, Bool) -> Void) {
         var result: Search = search
         page += 1
         let param: [String:Any] = ["query":text, "page":page, "size":30]
-        return apiManager.getAPI(param: param).flatMap { data -> Observable<Search> in
-            result.document += data.document
-            result.meta = data.meta
-            return Observable.just(result)
-        }
+        apiManager.getAPI(param: param).subscribe(
+            onNext: { data in
+                result.document += data.document
+                result.meta = data.meta
+                handler(result, true)
+            }, onError: { err in
+                print(err)
+                handler(search, false)
+            }).disposed(by: disposeBag)
     }
     
     
