ab5c0cb4e5ef53a9814602e2c7c2ae700819135a
sbear123 <sbear123@naver.com>
2021-06-30 09:46:36 +0900

    [UPDATE] CellStyle 정하는 코드 작성

--- SearchImage/Source/Modules/Main/View/Cell/MainCell.swift
+++ SearchImage/Source/Modules/Main/View/Cell/MainCell.swift
@@ -8,7 +8,7 @@
 import UIKit
 
 class MainCell: UICollectionViewCell {
-    @IBOutlet weak var image: UIImageView!
+    @IBOutlet var image: UIImageView!
     
     func update(imgUrl: String) {
         URLSession.shared.dataTask(with: URL(string: imgUrl)!) { (data, response, err) in
@@ -27,5 +27,19 @@ class MainCell: UICollectionViewCell {
                             self.image.image = image
                           },
                           completion: nil)
+    }
+    
+    func cellStyle() {
+        self.contentView.layer.cornerRadius = 10
+        self.contentView.layer.borderWidth = 1.0
+        self.contentView.layer.borderColor = UIColor.clear.cgColor
+        self.contentView.layer.masksToBounds = true
+        
+        self.layer.shadowColor = UIColor.lightGray.cgColor
+        self.layer.shadowOffset = CGSize(width: 0, height: 2.0)
+        self.layer.shadowRadius = 2.0
+        self.layer.shadowOpacity = 1.0
+        self.layer.masksToBounds = false
+        self.layer.shadowPath = UIBezierPath(roundedRect:self.bounds, cornerRadius:self.contentView.layer.cornerRadius).cgPath
     }
 }
